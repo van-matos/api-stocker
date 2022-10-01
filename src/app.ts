81f98a39814f60bf133a7508e01b62914749861a
van-matos <vanmatos@poli.ufrj.br>
2022-10-01 17:05:06 -0300

    :sparkles: feat: create authorization router

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,15 @@ import dotenv from "dotenv";
 import express from "express";
 import "express-async-errors";
 
+import router from "./routes/index";
+
 dotenv.config;
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use(router);
 
 export default app;
