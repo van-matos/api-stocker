91274da5b1bbeeac30129ac2fe426086588cdf1d
van-matos <vanmatos@poli.ufrj.br>
2022-10-02 17:29:55 -0300

    fix: add quantity to product schema

--- src/schemas/productSchema.ts
+++ src/schemas/productSchema.ts
@@ -4,5 +4,6 @@ export const productSchema = joi.object({
   barcode: joi.string().trim().required(),
   name: joi.string().trim().required(),
   description: joi.string().trim().required(),
+  quantity: joi.number().integer().min(1).required(),
   price: joi.number().precision(2).min(0).required(),
 });
