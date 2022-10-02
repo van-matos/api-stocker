fd2dc95606eecae7250a7dda42e3a4f9f75ec673
van-matos <vanmatos@poli.ufrj.br>
2022-10-01 22:46:37 -0300

    :sparkles: feat: add password verification

--- src/utils/passwordEncrypter.ts
+++ src/utils/passwordEncrypter.ts
@@ -4,6 +4,13 @@ function encryptPassword(password: string) {
   return bcrypt.hashSync(password, 10);
 }
 
+export function verifyPassword(
+  password: string,
+  hashedPassword: string
+): boolean {
+  return bcrypt.compareSync(password, hashedPassword);
+}
+
 export const passwordEncrypter = {
   encryptPassword,
 };
