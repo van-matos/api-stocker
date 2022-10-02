ebeb8a20cac5c5b553d30305b20a1036cb0ae2da
van-matos <vanmatos@poli.ufrj.br>
2022-10-02 19:51:22 -0300

    feat: add product update service

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -1,6 +1,6 @@
 import { authService } from "./authService";
 import { productRepository } from "../repositories/productRepository";
-import { INewProductData } from "../types/productTypes";
+import { INewProductData, IProductData } from "../types/productTypes";
 
 async function addProduct(userId: number, productData: INewProductData) {
   await authService.getUserById(userId);
@@ -9,6 +9,22 @@ async function addProduct(userId: number, productData: INewProductData) {
     ...productData,
     userId,
   });
+
+  return product;
+}
+
+async function updateProduct(
+  userId: number,
+  productId: number,
+  productData: Omit<IProductData, "userId">
+) {
+  await authService.getUserById(userId);
+
+  const dbProduct = await productRepository.findProductById(productId);
+
+  if (!dbProduct) throw { status: 404, message: "Product not found." };
+
+  const product = await productRepository.updateProduct(productId, productData);
 
   return product;
 }
